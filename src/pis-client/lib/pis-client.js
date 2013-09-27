60f3e160a6f8cd83bb68fde85de7f849a853c20e
mamad <mohammad.asgari@gmail.com>
2013-09-27 03:59:37 +0100

    Updated client and test

--- src/pis-client/lib/pis-client.js
+++ src/pis-client/lib/pis-client.js
@@ -1,18 +1,51 @@
 var thrift = require('thrift'),
     ImageService = require('./ImageService.js'),
     ttypes = require('./service_types.js');
+var util = require('util');
+var events = require('events');
 
-function PISClient(options, callback) {
-    var self = this;
-    if (!options) {
-        var options = {};
+var DEFAULTS = {
+    host: '127.0.0.1',
+    port: '3800'
+};
+
+function Client(options) {
+    options = options || {};
+    this.options = merge(options, DEFAULTS);
+}
+
+exports.Client = Client;
+
+util.inherits(Client, events.EventEmitter);
+
+
+Client.prototype.ping = function(callback) {
+    callback(new Error('Nothing yet'), null);
+    //callback(null, 0);
+};
+
+
+// main export function
+exports.createClient = function(options) {
+    return new Client(options);
+}
+
+
+// Helper functions
+
+// callback || noop borrowed from node/lib/fs.js
+function noop() {}
+
+
+function merge(a, b){
+    if (a && b) {
+        for (var key in b) {
+            if (typeof a[key] == 'undefined') {
+                a[key] = b[key];
+            } else if (typeof a[key] == 'object' && typeof b[key] == 'object') {
+                a[key] = merge(a[key], b[key]);
+            }
+        }
     }
-
+    return a;
 }
-
-module.exports = PISClient;
-
-PISClient.prototype.ping = function(callback) {
-    callback(new Error('Nothing yet'));
-}
-
--- src/pis-client/test/test.js
+++ src/pis-client/test/test.js
@@ -3,7 +3,12 @@ var pis = require('../index'),
     assert = require("assert");
 
 should.exist(pis);
-
-describe('test ping', function(){
-
+should.exist(pis.createClient);
+var client = pis.createClient();
+should.exist(client);
+should.exist(client.ping);
+client.ping(function(err, result){
+    should.not.exist(err);
+    should.exist(result);
+    result.should.equal(0);
 });
