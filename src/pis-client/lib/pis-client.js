4fd920d5e3cc94e4fb236f22a77938e40d94420d
mamad <mammad.asgari@gmail.com>
2013-10-01 21:35:18 +0100

    updated client module and test

--- src/pis-client/lib/pis-client.js
+++ src/pis-client/lib/pis-client.js
@@ -1,43 +1,133 @@
 var thrift = require('thrift'),
     ImageService = require('./ImageService.js'),
-    ttypes = require('./service_types.js');
-var util = require('util');
-var events = require('events');
+    ttypes = require('./service_types.js'),
+    util = require('util'),
+    events = require('events');
 
+/**
+ * default configurations for Peach Image Server Client
+ * @type {{host: string, port: number}}
+ */
 var DEFAULTS = {
     host: '127.0.0.1',
-    port: '3800'
+    port: 3800
 };
 
+/**
+ * Create a new client
+ * @param options
+ * @constructor
+ */
 function Client(options) {
     options = options || {};
     this.options = merge(options, DEFAULTS);
 }
 
+/**
+ *
+ * @type {Function}
+ */
 exports.Client = Client;
 
 util.inherits(Client, events.EventEmitter);
 
 
-Client.prototype.ping = function(callback) {
-    callback(new Error('Nothing yet'), null);
-    //callback(null, 0);
+/**
+ * Establish a connection. Re-emits 'connect', 'error' and 'end' events from the underlaying connection
+ * @param callback - in format function(err) to receive error message
+ */
+Client.prototype.connect = function (callback) {
+    var self = this;
+    var connection = thrift.createConnection(self.options.host, self.options.port, {transport: thrift.TFramedTransport})
+        .on('error',function (e) {
+            self.emit('error', e);
+            callback(e);
+        }).on('connect',function () {
+            self.emit('connect');
+        }).on('end', function (e) {
+            self.emit('end');
+        });
+
+    var thriftClient = thrift.createClient(ImageService, connection);
+
+    self.connection = connection;
+    self.thriftClient = thriftClient;
+};
+
+/**
+ * Close the connection
+ * @param callback - in format function(err) to receive error message
+ */
+Client.prototype.end = function (callback) {
+    if (this.connection) {
+        this.connection.end(callback);
+    } else {
+        callback(new Error('Not connected yet.'));
+    }
+};
+
+/**
+ * ping function, to check server health status, 0 means server is okay.
+ * @param callback in format function(err, result), result is an integer
+ */
+Client.prototype.ping = function (callback) {
+    var self = this;
+    try {
+        this.thriftClient.ping(callback);
+    } catch (e) {
+        self.emit('error', e);
+        callback(e, null);
+    }
+};
+
+/**
+ * resize function, to resize
+ * @param callback in format function(err, result), result is an integer
+ */
+
+/**
+ * resize function, to resize the input image
+ * @param imageData input image binary buffer
+ * @param width requested width
+ * @param height requested height
+ * @param callback in format function(err, resizedImageData, resizedWidth, resizedHeight)
+ */
+Client.prototype.resize = function (imageData, width, height, callback) {
+    var self = this;
+    try {
+        var request = new ttypes.TImage({data: imageData, width: width, height: height});
+        this.thriftClient.resize(request, function (err, response) {
+            if (err) {
+                callback(err, null, null, null);
+            } else if (response.error) {
+                callback(response.error, null, null, null);
+            } else if (response.result) {
+                callback(null, response.result.data, response.result.width, response.result.height);
+            } else {
+                callback('Illegal State - Empty response', null, null, null);
+            }
+        });
+    } catch (e) {
+        self.emit('error', e);
+        callback(e, null, null, null);
+    }
 };
 
 
 // main export function
-exports.createClient = function(options) {
+exports.createClient = function (options) {
     return new Client(options);
-}
+};
 
 
 // Helper functions
 
 // callback || noop borrowed from node/lib/fs.js
-function noop() {}
+function noop() {
+}
 
 
-function merge(a, b){
+function merge(a, b) {
     if (a && b) {
         for (var key in b) {
             if (typeof a[key] == 'undefined') {
--- src/pis-client/test/test.js
+++ src/pis-client/test/test.js
@@ -1,14 +1,87 @@
 var pis = require('../index'),
     should = require('should'),
+    fs = require('fs'),
     assert = require("assert");
 
-should.exist(pis);
-should.exist(pis.createClient);
-var client = pis.createClient();
-should.exist(client);
-should.exist(client.ping);
-client.ping(function(err, result){
-    should.not.exist(err);
-    should.exist(result);
-    result.should.equal(0);
-});
+module.exports = {
+    setUp: function (callback) {
+        var testSelf = this;
+        should.exist(pis.createClient);
+        this.client = pis.createClient();
+        should.exist(this.client);
+
+        this.client.on('connect', function () {
+            testSelf.connected = true;
+        });
+        this.client.on('error', function (e) {
+            console.log('Error in client:', e);
+            callback(e);
+        });
+
+        this.client.on('end', function () {
+            console.log('client ended.');
+        });
+
+        should.exist(this.client.connect);
+
+        this.client.connect(function (err) {
+            test.equals(err, null, 'No error should be thrown.' + err);
+        });
+        callback();
+    },
+
+    tearDown: function (callback) {
+        should.exist(this.client.end);
+        if (this.connected) {
+            this.client.end(function (err) {
+                console.log('Error in tearDown:', err);
+                callback(err);
+            });
+        }
+        callback();
+    },
+
+    testPing: function (test) {
+        should.exist(this.client.ping);
+        this.client.ping(function (err, result) {
+                test.equals(err, null, 'No error should be thrown.' + err);
+                test.equals(result, 0, 'Ping result must be 0');
+                test.done();
+            }
+        );
+    },
+
+    testResize: function(test) {
+        should.exist(this.client.resize);
+        var buff = new Buffer('abc');
+        this.client.resize(buff, 100, 100, function(err, resizeData, w, h){
+            should.exist(err);
+            console.log('error', err, '\n');
+            test.done();
+        });
+    },
+
+    testResizeRealImage : function(test) {
+        //test real image
+        var pisClient = this.client;
+        var tester = test;
+        fs.readFile('girl.jpg', function(fsErr, data){
+            if (fsErr) {
+                console.log('error in reading test image file.\n');
+                tester.done();
+            } else {
+                tester.ok(data);
+                tester.ok(data.length > 0);
+                pisClient.resize(data, 100, 80, function(err, resizeData, w, h){
+                    should.not.exist(err);
+                    tester.ok(resizeData, 'Resized data should not be null.');
+                    tester.ok(resizeData.length > 0, 'Resized data length should not be 0.');
+                    tester.ok(w > 0, 'Resized width should not be 0.');
+                    tester.ok(h > 0, 'Resized height should not be 0.');
+                    tester.done();
+                });
+            }
+        });
+    }
+
+}
